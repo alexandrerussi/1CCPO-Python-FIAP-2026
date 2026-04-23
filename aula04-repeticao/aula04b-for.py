58df113119e0c3e0840ca2c44cfa53425ae71574
alexandrerussi <alexandrerussijunior@gmail.com>
2026-04-23 20:55:39 -0300

    aula 04 repetição

---
+++ aula04-repeticao/aula04b-for.py
@@ -0,0 +1,23 @@
+# for c_msc in range(3):
+#     print(f"Música {c_msc}")
+#
+# # de 1 até 11, pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+
+# ATIVIDADE 3
+# qtd_musicas = int(input("Quantas músicas vc tem na playlist? (db): "))
+#
+# for i in range(qtd_musicas):
+#     print(f"Música {i}")
+
+# LAÇOS aninhados
+# REP. encadeada
+
+for i in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
+
+
+
+
